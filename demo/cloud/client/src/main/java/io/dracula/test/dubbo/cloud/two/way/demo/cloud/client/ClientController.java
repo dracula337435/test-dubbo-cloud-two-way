c10cbd459537081b81858e10ed610e79de5f1cc3
dk <dracula337435@163.com>
2020-05-08 16:56:20 +0800

    增加cloud的client方用RestTmplate

--- demo/cloud/client/src/main/java/io/dracula/test/dubbo/cloud/two/way/demo/cloud/client/ClientController.java
+++ demo/cloud/client/src/main/java/io/dracula/test/dubbo/cloud/two/way/demo/cloud/client/ClientController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author dk
@@ -24,6 +25,18 @@ public class ClientController {
         msgInClient.setMsg(msg);
         Echo.MsgInClient echoMsg = echo.echo(msgInClient);
         logger.info("echoFeignCloud将要返回："+echoMsg.getMsg());
+        return echoMsg;
+    }
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("echoTemplateCloud")
+    public Echo.MsgInClient echoTemplateCloud(@RequestParam(name="msg", defaultValue="hello") String msg){
+        Echo.MsgInClient msgInClient = new Echo.MsgInClient();
+        msgInClient.setMsg(msg);
+        Echo.MsgInClient echoMsg = restTemplate.postForObject("http://test-cloud-server/echo", msgInClient, Echo.MsgInClient.class);
+        logger.info("echoTemplateCloud将要返回："+echoMsg.getMsg());
         return echoMsg;
     }
 
--- demo/cloud/client/src/main/java/io/dracula/test/dubbo/cloud/two/way/demo/cloud/client/CloudClientMainApp.java
+++ demo/cloud/client/src/main/java/io/dracula/test/dubbo/cloud/two/way/demo/cloud/client/CloudClientMainApp.java
@@ -2,8 +2,11 @@ package io.dracula.test.dubbo.cloud.two.way.demo.cloud.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.tsf.annotation.EnableTsf;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author dk
@@ -19,6 +22,12 @@ public class CloudClientMainApp {
      */
     public static void main(String[] args){
         SpringApplication.run(CloudClientMainApp.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 
 }
